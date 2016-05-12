4588e2e5531d5f134f49055409710ffac1431864
Jen <contact.vandepool@gmail.com>
2016-05-12 18:27:14 +0000

    document form partial, create new document

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -5,10 +5,22 @@ class DocsController < ApplicationController
     def show
     end
     
+    # create new document with its parameters title & content
     def create
+        @doc = Doc.new(doc_params)
+        
+        # if document is sucessfully saved, redirect to the new document
+        if @doc.save
+            redirect_to @doc
+        # if document is not saved, render form again to create new document
+        else   
+            render 'new'
+        end    
     end
     
+    # new document view
     def new
+        @doc = Doc.new
     end
     
     def update
@@ -26,6 +38,7 @@ class DocsController < ApplicationController
         end
         
         def doc_params
+            params.require(:doc).permit(:title, :content)
         end    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  # view welcome index on main page
   root 'welcome#index'
   
+  # create routes for documents
   resources :docs
 end
